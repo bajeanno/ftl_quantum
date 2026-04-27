f90a5d7d91cec9168df1b5ced9e2b831c491c01e
Basile Jeannot <bajeanno@student.42lyon.fr>
2026-04-27 14:38:07 +0200

    feat: changed get_results in ex02 to avoid re-running program on quantum computer

--- ex02/get_results.py
+++ ex02/get_results.py
@@ -3,7 +3,9 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 import matplotlib.pyplot as plt
 
 service = QiskitRuntimeService()
-job = service.job(input("enter job id: "))
+# job_id = input("enter job id: ")
+job_id = "d7laf8a4lglc7380cl80"
+job = service.job(job_id)
 count_result01 = job.result()[0].data.meas.get_counts()
 nbshots = 500
 two_qubits_superposition_entanglement = {i: j/ nbshots for i, j in count_result01.items()} # add some normalisation to comply with subject
